753d8ebd890e24560211b7cece580c04aede978c
Vubon Roy <vubon.roy@gmail.com>
2017-06-10 15:42:42 +0600

    mixins, generics system in view model

--- snippets/views.py
+++ snippets/views.py
@@ -7,15 +7,119 @@ from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 '''
-#Api View Add for function base views
-
+'''
+#Api View Add for function base views for function based views
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+'''
+'''
+# for class based view api 
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+'''
+'''
+# Using mixins and generics in view system 
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import mixins
+from rest_framework import generics
+'''
+# Generics view system 
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+'''
+
+#Using Mixins system for view model in class based view  
+class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+
+	def get(self, request, *args, **kwargs):
+		return self.list(request, *args, **kwargs)
+
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
+
+class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+
+	def get(self, request, *args, **kwargs):
+		return self.retrieve(request, *args, **kwargs)
+
+	def put(self, request, *args, **kwargs):
+		return self.update(request, *args, **kwargs)
+
+	def delete(self, request, *args, **kwargs):
+		return self.destroy(request, *args, **kwargs)
+
+
+'''
+
+'''
+#Class based view 
+class SnippetList(APIView):
+	#List all snippets or create a new snippet
+
+	def get(self, request, format=None):
+		snippets = Snippet.objects.all()
+		serializer = SnippetSerializer(snippets, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, format=None):
+		serializer = SnippetSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SnippetDetail(APIView):
+	#Retrieve, update or delete a snippet instance
+	def get_object(self, pk):
+		try:
+			return Snippet.objects.get(pk=pk)
+		except Snippet.DoesNotExist:
+			raise Http404
+
+	def get(self, request, pk, format=None):
+		snippet = self.get_object(pk)
+		serializer = SnippetSerializer(snippet, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def delete(self, request, pk, format=None):
+		snippet = self.get_object(pk)
+		snippet.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+		
+
+'''
+
+'''
+# This is a functions based view :D
 @api_view(['GET', 'POST'])
 def snippet_list(request, formate=None):
 	#List all snippets or create a new snippet
@@ -60,8 +164,9 @@ def snippet_detail(request, pk, format=None):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
 '''
+'''
+#csrf_exempt base view system 
 @csrf_exempt
 def snippet_list(request):
 	# List all code snippets or create a new snippet
